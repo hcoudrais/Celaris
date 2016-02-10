f0f08691404ec7fddc0bc6b956a6d9936d209a8f
root <kyklos@gmail.com>
2016-02-10 23:36:23 +0100

    add fields in Ship entity

--- src/Celaris/Game/Entity/Ship.php
+++ src/Celaris/Game/Entity/Ship.php
@@ -23,12 +23,144 @@ class Ship
     protected $name;
 
     /**
-     * @ORM\OneToMany(targetEntity="Module", mappedBy="moduleId")
-     */
-    protected $modules;
-
-    /**
      * @ORM\OneToMany(targetEntity="Celaris", mappedBy="celarisId")
      */
-    protected $celarisId;
+    protected $celaris;
+
+    /**
+     * @ORM\Column(name="Attack", type="integer")
+     */
+    protected $attack;
+
+    /**
+     * @ORM\Column(name="Defense", type="integer")
+     */
+    protected $defense;
+    
+    /**
+     * @ORM\Column(name="Freight", type="integer")
+     */
+    protected $freight;
+
+    /**
+     * @ORM\Column(name="L-com", type="boolean")
+     */
+    protected $lCom;
+
+    /**
+     * @ORM\Column(name="Camouflage, type="boolean")
+     */
+    protected $camouflage;
+
+    /**
+     * @ORM\Column(name="Colonization", type="boolean")
+     */
+    protected $colonization;
+
+    /**
+     * @ORM\Column(name="Quantity", type="integer")
+     */
+    protected $quantity;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Fuselage", mappedBy="fuselageId")
+     */
+    protected $fuselage;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Model", mappedBy="modelId")
+     */
+    protected $model;
+
+    function getShipId() {
+        return $this->shipId;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getCelaris() {
+        return $this->celaris;
+    }
+
+    function getAttack() {
+        return $this->attack;
+    }
+
+    function getDefense() {
+        return $this->defense;
+    }
+
+    function getFreight() {
+        return $this->freight;
+    }
+
+    function getLCom() {
+        return $this->lCom;
+    }
+
+    function getCamouflage() {
+        return $this->camouflage;
+    }
+
+    function getColonization() {
+        return $this->colonization;
+    }
+
+    function getQuantity() {
+        return $this->quantity;
+    }
+
+    function getFuselage() {
+        return $this->fuselage;
+    }
+
+    function getModel() {
+        return $this->model;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function setCelaris($celaris) {
+        $this->celaris = $celaris;
+    }
+
+    function setAttack($attack) {
+        $this->attack = $attack;
+    }
+
+    function setDefense($defense) {
+        $this->defense = $defense;
+    }
+
+    function setFreight($freight) {
+        $this->freight = $freight;
+    }
+
+    function setLCom($lCom) {
+        $this->lCom = $lCom;
+    }
+
+    function setCamouflage($camouflage) {
+        $this->camouflage = $camouflage;
+    }
+
+    function setColonization($colonization) {
+        $this->colonization = $colonization;
+    }
+
+    function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+
+    function setFuselage($fuselage) {
+        $this->fuselage = $fuselage;
+    }
+
+    function setModel($model) {
+        $this->model = $model;
+    }
 }
